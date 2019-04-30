a465465ae530c014ff353e4451e1e3a7965d9f60
Eduardo <eduardo.mariot@gmail.com>
2019-04-30 01:06:50 -0300

    Model de descontos

--- app/Models/Discount.js
+++ app/Models/Discount.js
@@ -1,9 +1,19 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Discount extends Model {
+  static get table() {
+    return "coupon_order";
+  }
+
+  orden() {
+    return this.belongsTo("App/Models/Order", "order_id", "id");
+  }
+  coupon() {
+    return this.belongsTo("App/Models/Coupon", "coupon_id", "id");
+  }
 }
 
-module.exports = Discount
+module.exports = Discount;

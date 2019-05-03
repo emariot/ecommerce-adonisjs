82d6d619bb6e386382ef47543b0336b50b93068f
Eduardo <eduardo.mariot@gmail.com>
2019-05-02 23:51:02 -0300

    Correção

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -7,15 +7,15 @@ const Route = use("Route");
  * Rota de Autenticação
  */
 Route.group(() => {
-  Route.post("register", "AuthControler.register").as("auth.register");
-  Route.post("login", "AuthControler.login").as("auth.login");
-  Route.post("refresh", "AuthControler.refresh").as("auth.refresh");
-  Route.post("logout", "AuthControler.logout").as("auth.logout");
+  Route.post("register", "AuthController.register").as("auth.register");
+  Route.post("login", "AuthController.login").as("auth.login");
+  Route.post("refresh", "AuthController.refresh").as("auth.refresh");
+  Route.post("logout", "AuthController.logout").as("auth.logout");
 
   // restore password routes
-  Route.post("reset-password", "AuthControler.forgot").as("auth.forgot");
-  Route.get("reset-password", "AuthControler.remember").as("auth.remember");
-  Route.put("reset-password", "AuthControler.reset").as("auth.reset");
+  Route.post("reset-password", "AuthController.forgot").as("auth.forgot");
+  Route.get("reset-password", "AuthController.remember").as("auth.remember");
+  Route.put("reset-password", "AuthController.reset").as("auth.reset");
 })
   .prefix("v1/auth")
   .namespace("Auth");

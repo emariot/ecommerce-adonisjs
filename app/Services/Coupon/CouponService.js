e62285c78afb8abe679ebcb91c4b4bc75bea566c
Eduardo <eduardo.mariot@gmail.com>
2019-05-05 01:36:57 -0300

    Services: Coupon Service

---
+++ app/Services/Coupon/CouponService.js
@@ -0,0 +1,30 @@
+"use strict";
+
+class CouponService {
+  constructor(model, trx = null) {
+    this.model = model;
+    this.trx = trx;
+  }
+
+  async syncUsers(users) {
+    if (!Array.isArray(users)) {
+      return false;
+    }
+    await this.model.users().sync(users, null, this.trx);
+  }
+
+  async syncOrders(ordens) {
+    if (!Array.isArray(ordens)) {
+      return false;
+    }
+    await this.model.ordens().sync(ordens, null, this.trx);
+  }
+  async syncProducts(products) {
+    if (!Array.isArray(products)) {
+      return false;
+    }
+    await this.model.products().sync(products, null, this.trx);
+  }
+}
+
+module.exports = CouponService;

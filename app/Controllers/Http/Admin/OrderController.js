308f9ffcc5607968522c93695d73e77f31a259e9
Eduardo <eduardo.mariot@gmail.com>
2019-05-06 13:21:53 -0300

    Metodo para aplicar desconto

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -7,6 +7,8 @@
 const Order = use("App/Models/Order");
 const Database = use("Database");
 const Service = use("App/Service/Order/OrderService");
+const Coupon = use("App/Models/Coupon");
+const Discount = use("App/Models/Discount");
 
 /**
  * Resourceful controller for interacting with orders
@@ -137,6 +139,45 @@ class OrderController {
         message: "Erro ao deletar este pedido. Tente mais tarde!"
       });
     }
+  }
+
+  async applyDiscount({ params: { id }, request, response }) {
+    const { code } = request.all();
+    const coupon = await Coupon.findByOrFail("code", code.toUpperCase());
+    const order = await Order.findOrFail(id);
+    var discount,
+      info = {};
+    try {
+      const service = new Service(order);
+      const canAddDiscount = await service.canApplyDiscount(coupon);
+      const orderDiscounts = await order.coupons().getCount();
+      const cammApplyToOrder =
+        orderDiscounts < 1 || (orderDiscounts >= 1 && coupon.recursive);
+      if (canAddDiscount && cammApplyToOrder) {
+        discount = await Discount.findOrCreate({
+          order_id: order.id,
+          coupon_id: coupon.id
+        });
+        info.menssage = "Cupom aplicado com sucesso!";
+        info.success = true;
+      } else {
+        info.message = "Não foi possível aplicar este cupom";
+        info.success = false;
+      }
+
+      return response.send(order, info);
+    } catch (error) {
+      return response.status(400).send({
+        message: "Erro ao aplicar o cupom"
+      });
+    }
+  }
+
+  async removeDiscount({ request, response }) {
+    const { discount_id } = request.all();
+    const discount = await Discount.findOrFail(discount_id);
+    await discount.delete();
+    return response.status(204).send();
   }
 }
 

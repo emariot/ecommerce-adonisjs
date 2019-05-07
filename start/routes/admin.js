23b8f99b56fac33fbac7e906f99bd37523d07bbc
Eduardo <eduardo.mariot@gmail.com>
2019-05-07 00:55:38 -0300

    Validator: admin and auth

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -11,12 +11,24 @@ Route.group(() => {
   /**
    * Categories and Product resource routes
    */
-  Route.resource("categories", "CategoryController").apiOnly();
+  Route.resource("categories", "CategoryController")
+    .apiOnly()
+    .validator(
+      new Map([
+        [["categories.store"], ["Admin/StoreCategory"]],
+        [["categories.update"], ["Admin/StoreCategory"]]
+      ])
+    );
+
   Route.resource("products", "ProductController").apiOnly();
   Route.resource("coupons", "CouponController").apiOnly();
   Route.post("orders/:id/discount", "OrderController.applyDiscount");
   Route.delete("orders/:id/discount", "OrderController.removeDiscount");
-  Route.resource("orders", "OrderController").apiOnly();
+
+  Route.resource("orders", "OrderController")
+    .apiOnly()
+    .validator(new Map([[["orders.store"], ["Admin/StoreOrder"]]]));
+
   Route.resource("images", "ImageController").apiOnly();
   Route.resource("users", "UserController").apiOnly();
 })
